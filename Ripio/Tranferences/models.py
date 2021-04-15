d6f000d2fbeab4dff6eac3e5613ac392cf5fe469
WarcaldianPlace <nachovidondo@gmail.com>
2021-04-15 15:47:57 -0300

    Tranferences model

---
+++ Ripio/Tranferences/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from Currencies.models import Currency
+
+
+class Transfer(models.Model):
+    id = models.AutoField(primary_key=True)
+    origin_account = models.CharField(
+        max_length=200, verbose_name="Cuenta de origen"
+        )
+    destination_account = models.CharField(
+        max_length=200,verbose_name="Cuenta de destino"
+        )
+    creation_date = models.DateTimeField(auto_now=True, verbose_name="Fecha")
+    currency = models.ForeignKey(
+        Currency, on_delete=models.CASCADE, verbose_name="Moneda"
+        )
+    amount = models.FloatField(verbose_name="Cantidad")
+    
+    class Meta:
+        verbose_name="Transferencia"
+        verbose_name_plural = "Transferencias"
+        
+    def __str__(self):
+        return str(self.id)
--- Ripio/Users/models.py
+++ Ripio/Users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+
 class UserManager(BaseUserManager):
     def create_user(self,email,username,name,surname,password=None):
         if not email:
