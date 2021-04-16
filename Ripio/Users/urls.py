0ea8c6b541945c063c5acb0bb460d417012ce73e
WarcaldianPlace <nachovidondo@gmail.com>
2021-04-16 13:11:52 -0300

    Reset password and email config

--- Ripio/Users/urls.py
+++ Ripio/Users/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
+from django.contrib.auth  import views as auth_views 
 from .views import UserRegister, IndexView, Login
+
+
+
 urlpatterns = [
     path('register/',UserRegister.as_view(), name="register"),
     path('index/',IndexView.as_view(), name='index'),
+    path('reset_password/',auth_views.PasswordResetView.as_view(),
+         name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), 
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(), 
+         name="password_reset_complete")
     ]
--- Ripio/config/settings/base.py
+++ Ripio/config/settings/base.py
@@ -112,3 +112,12 @@ STATIC_URL = '/static/'
 
 #Bootsrap 4 Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+#Email configurations
+
+EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER =  'ripiocurrencies@gmail.com'
+EMAIL_HOST_PASSWORD = 'Ripio123' 
